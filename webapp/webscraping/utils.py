2ef072e23cd41dd4e39a4f1dc0f9547d2eb3b9e5
TinyProgrammer <kubagrowy@gamil.com>
2025-06-11 17:21:22 +0200

    fix

--- webapp/webscraping/utils.py
+++ webapp/webscraping/utils.py
@@ -8,23 +8,23 @@ from urllib.parse import urlparse
 WIKI_BASE = "https://en.wikipedia.org"
 
 
+def is_article_url(text: str) -> list[bool]:
+    return [
+        text.startswith("http"),
+        text.find(".wikipedia.org/wiki/") != -1,
+    ]
+
+
 def article_url_by_name(article_name: str) -> str:
+    if all(is_article_url(article_name)):
+        return article_name
+
     title = article_name.strip().replace(" ", "_")
     return f"{WIKI_BASE}/wiki/{title}"
 
 
-def article_name_by_url(article_url: str | None) -> str | None:
-    article_url = article_url.strip()
-    if not article_url:
+def article_name_by_url(article_url: str) -> str | None:
+    if not all(is_article_url(article_url)):
         return None
 
-    validations = [
-        article_url.startswith("http"),
-        article_url.find(".wikipedia.org/wiki/") != -1,
-    ]
-    if not all(validations):
-        return None
-
-    return (
-        article_url.split("/wiki/")[-1].replace("_", " ") if all(validations) else None
-    )
+    return article_url.split("/wiki/")[-1].replace("_", " ")
