d408f14c3f7dac07a9427f7a28bcbe613c2f344d
mateyko22 <73527305+mateyko22@users.noreply.github.com>
2026-03-15 15:24:23 +0100

    add wikispeedrun link generation and integrate into tracker API

--- webapp/wikipedia_webscraping/utils.py
+++ webapp/wikipedia_webscraping/utils.py
@@ -8,15 +8,15 @@ from urllib.parse import urlparse
 WIKI_BASE = "https://en.wikipedia.org"
 
 
-def is_article_url(text: str) -> list[bool]:
-    return [
+def is_article_url(text: str) -> bool:
+    return all([
         text.startswith("http"),
         text.find(".wikipedia.org/wiki/") != -1,
-    ]
+    ])
 
 
 def article_url_by_name(article_name: str) -> str:
-    if all(is_article_url(article_name)):
+    if is_article_url(article_name):
         return article_name
 
     title = article_name.strip().replace(" ", "_")
@@ -24,7 +24,7 @@ def article_url_by_name(article_name: str) -> str:
 
 
 def article_name_by_url(article_url: str) -> str | None:
-    if not all(is_article_url(article_url)):
+    if not is_article_url(article_url):
         return None
 
     return article_url.split("/wiki/")[-1].replace("_", " ")
