f9ca766be9ae969f70e42768fc59d15337996152
d4krzyk <50460458+d4krzyk@users.noreply.github.com>
2026-03-19 23:55:31 +0100

    (not tested in game only in menu build) added screen resolution change in options

--- WikiRoomsProjectUnity/Assets/Scripts/MainMenu/MenuController.cs
+++ WikiRoomsProjectUnity/Assets/Scripts/MainMenu/MenuController.cs
@@ -1,8 +1,33 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+#if UNITY_STANDALONE_WIN && !UNITY_EDITOR
+using System;
+using System.Runtime.InteropServices;
+#endif
 
 public class MenuController : MonoBehaviour
 {
+#if UNITY_STANDALONE_WIN && !UNITY_EDITOR
+    const int SW_MAXIMIZE = 3;
+
+    [DllImport("user32.dll")]
+    static extern IntPtr GetActiveWindow();
+
+    [DllImport("user32.dll")]
+    static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll")]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+#endif
+
+    enum DisplayModeOption
+    {
+        Fullscreen = 0,
+        Windowed = 1
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject mainMenuPanel;
     public GameObject playMenuPanel;
@@ -11,8 +36,10 @@ public class MenuController : MonoBehaviour
 
     [Header("Settings")]
     public Toggle toggleGenAISettings;
+    public TMP_Dropdown resolutionDropdown;
 
     const string GenAIEnabledKey = "GenAITexturesEnabled";
+    const string DisplayModeKey = "DisplayMode";
 
 
 
@@ -21,6 +48,8 @@ public class MenuController : MonoBehaviour
     public AudioClip hoverSound;
     public AudioClip clickSound;
     [Range(0f,1f)] public float uiVolume = 1f;
+
+    Coroutine pendingDisplayModeRoutine;
 
     void Awake()
     {
@@ -37,6 +66,18 @@ public class MenuController : MonoBehaviour
             toggleGenAISettings.isOn = enabled;
             toggleGenAISettings.onValueChanged.AddListener(OnGenAIToggleChanged);
         }
+
+        EnsureResolutionDropdownReference();
+        if (resolutionDropdown != null)
+        {
+            resolutionDropdown.onValueChanged.RemoveListener(OnResolutionDropdownChanged);
+            resolutionDropdown.onValueChanged.AddListener(OnResolutionDropdownChanged);
+            InitializeDisplayMode();
+        }
+        else
+        {
+            Debug.LogWarning("[MenuController] Resolution dropdown is missing. Fullscreen/windowed mode will not be configurable from Options.");
+        }
     }
 
     void OnDestroy()
@@ -45,12 +86,107 @@ public class MenuController : MonoBehaviour
         {
             toggleGenAISettings.onValueChanged.RemoveListener(OnGenAIToggleChanged);
         }
+
+        if (resolutionDropdown != null)
+        {
+            resolutionDropdown.onValueChanged.RemoveListener(OnResolutionDropdownChanged);
+        }
+
+        if (pendingDisplayModeRoutine != null)
+        {
+            StopCoroutine(pendingDisplayModeRoutine);
+            pendingDisplayModeRoutine = null;
+        }
     }
 
     void OnGenAIToggleChanged(bool isOn)
     {
         PlayerPrefs.SetInt(GenAIEnabledKey, isOn ? 1 : 0);
         PlayerPrefs.Save();
+    }
+
+    void EnsureResolutionDropdownReference()
+    {
+        if (resolutionDropdown != null)
+            return;
+
+        if (settingsMenuPanel != null)
+            resolutionDropdown = settingsMenuPanel.GetComponentInChildren<TMP_Dropdown>(true);
+
+        if (resolutionDropdown == null)
+            resolutionDropdown = FindFirstObjectByType<TMP_Dropdown>(FindObjectsInactive.Include);
+    }
+
+    void InitializeDisplayMode()
+    {
+        int dropdownValue = PlayerPrefs.HasKey(DisplayModeKey)
+            ? Mathf.Clamp(PlayerPrefs.GetInt(DisplayModeKey), 0, 1)
+            : (IsWindowedMode(Screen.fullScreenMode) ? (int)DisplayModeOption.Windowed : (int)DisplayModeOption.Fullscreen);
+
+        resolutionDropdown.SetValueWithoutNotify(dropdownValue);
+
+        if (PlayerPrefs.HasKey(DisplayModeKey))
+        {
+            ApplyDisplayMode((DisplayModeOption)dropdownValue, savePreference: false);
+        }
+    }
+
+    void OnResolutionDropdownChanged(int selectedIndex)
+    {
+        DisplayModeOption displayMode = selectedIndex == (int)DisplayModeOption.Windowed
+            ? DisplayModeOption.Windowed
+            : DisplayModeOption.Fullscreen;
+
+        ApplyDisplayMode(displayMode, savePreference: true);
+    }
+
+    void ApplyDisplayMode(DisplayModeOption displayMode, bool savePreference)
+    {
+        Resolution monitorResolution = Screen.currentResolution;
+
+        if (pendingDisplayModeRoutine != null)
+        {
+            StopCoroutine(pendingDisplayModeRoutine);
+            pendingDisplayModeRoutine = null;
+        }
+
+        if (displayMode == DisplayModeOption.Windowed)
+        {
+            Screen.SetResolution(monitorResolution.width, monitorResolution.height, FullScreenMode.Windowed);
+            pendingDisplayModeRoutine = StartCoroutine(MaximizeWindowNextFrame());
+        }
+        else
+        {
+            Screen.SetResolution(monitorResolution.width, monitorResolution.height, FullScreenMode.ExclusiveFullScreen);
+        }
+
+        if (savePreference)
+        {
+            PlayerPrefs.SetInt(DisplayModeKey, (int)displayMode);
+            PlayerPrefs.Save();
+        }
+    }
+
+    bool IsWindowedMode(FullScreenMode fullScreenMode)
+    {
+        return fullScreenMode == FullScreenMode.Windowed || fullScreenMode == FullScreenMode.MaximizedWindow;
+    }
+
+    IEnumerator MaximizeWindowNextFrame()
+    {
+        yield return null;
+        yield return new WaitForEndOfFrame();
+
+#if UNITY_STANDALONE_WIN && !UNITY_EDITOR
+        IntPtr windowHandle = GetActiveWindow();
+        if (windowHandle == IntPtr.Zero)
+            windowHandle = GetForegroundWindow();
+
+        if (windowHandle != IntPtr.Zero)
+            ShowWindow(windowHandle, SW_MAXIMIZE);
+#endif
+
+        pendingDisplayModeRoutine = null;
     }
 
     // metody dostępne z zewnątrz do odtwarzania dźwięków UI
--- WikiRoomsProjectUnity/Assets/Scripts/WikiipediaRoomClickBehaviour.cs
+++ WikiRoomsProjectUnity/Assets/Scripts/WikiipediaRoomClickBehaviour.cs
@@ -2,13 +2,29 @@ using UnityEngine;
 using LogicUI.FancyTextRendering;
 using System.Text.RegularExpressions;
 using System;
+using TMPro;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(TextLinkHelper))]
 [DisallowMultipleComponent]
 public class WikiipediaRoomClickBehaviour : MonoBehaviour
 {
+    const string WikipediaArticlePattern = @"https:\/\/en\.wikipedia\.org\/wiki\/([^#]+)(?:#(.+))?";
+    const float TooltipMaxWidth = 360f;
+    static readonly Vector2 TooltipPadding = new Vector2(20f, 16f);
+    static readonly Vector2 TooltipOffset = new Vector2(18f, -18f);
+
     RoomsController _roomsController;
     Logger logger;
+    TextLinkHelper linkHelper;
+    TMP_Text pageText;
+    Canvas parentCanvas;
+
+    static RectTransform tooltipRoot;
+    static Image tooltipBackground;
+    static TextMeshProUGUI tooltipLabel;
+    static Canvas tooltipCanvas;
+    static WikiipediaRoomClickBehaviour activeTooltipOwner;
 
     [Header("SFX kliknięcia linku")]
     public AudioClip clickSound;
@@ -30,6 +46,9 @@ public class WikiipediaRoomClickBehaviour : MonoBehaviour
 
     private void Awake()
     {
+        linkHelper = GetComponent<TextLinkHelper>();
+        pageText = GetComponent<TMP_Text>();
+
         if (userTransform == null)
         {
             try
@@ -43,21 +62,59 @@ public class WikiipediaRoomClickBehaviour : MonoBehaviour
             }
         }
 
-        GetComponent<TextLinkHelper>().OnLinkClicked += ClickOnLink;
+        if (linkHelper != null)
+        {
+            linkHelper.OnLinkClicked += ClickOnLink;
+            linkHelper.OnLinkHovered += ShowLinkTooltip;
+            linkHelper.OnLinkHoverEnded += HideLinkTooltip;
+        }
+
         logger = FindAnyObjectByType<Logger>();
+    }
+
+    private void Update()
+    {
+        if (activeTooltipOwner == this)
+        {
+            UpdateTooltipPosition();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (activeTooltipOwner == this)
+        {
+            HideTooltipImmediate();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (linkHelper != null)
+        {
+            linkHelper.OnLinkClicked -= ClickOnLink;
+            linkHelper.OnLinkHovered -= ShowLinkTooltip;
+            linkHelper.OnLinkHoverEnded -= HideLinkTooltip;
+        }
+
+        if (activeTooltipOwner == this)
+        {
+            HideTooltipImmediate();
+        }
     }
 
     private void ClickOnLink(string link)
     {
+        HideLinkTooltip();
+
         // Odtwórz dźwięk w lokalizacji użytkownika
         PlayClickSoundAtUser();
 
         link = CloseParentheses(link);
-        string pattern = @"https:\/\/en\.wikipedia\.org\/wiki\/([^#]+)(?:#(.+))?";
 
         logger.LogOnLinkClick(link, Time.time);
 
-        Match match = Regex.Match(link, pattern);
+        Match match = Regex.Match(link, WikipediaArticlePattern);
         if (match.Success)
         {
             PlayerController playerController = FindAnyObjectByType<PlayerController>();
@@ -72,6 +129,167 @@ public class WikiipediaRoomClickBehaviour : MonoBehaviour
             roomsController.secondElongatedRoom.GenerateRoom(roomsController.secondElongatedRoom.articleName, roomsController, onClick: true);
             roomsController.AddNextRoomToHistory(roomsController.secondElongatedRoom.articleName);
         }
+    }
+
+    private void ShowLinkTooltip(string link)
+    {
+        string articleTitle = ExtractArticleTitle(link);
+        if (string.IsNullOrEmpty(articleTitle))
+        {
+            HideLinkTooltip();
+            return;
+        }
+
+        EnsureTooltipExists();
+        if (tooltipRoot == null || tooltipLabel == null)
+            return;
+
+        activeTooltipOwner = this;
+        tooltipLabel.text = articleTitle;
+        UpdateTooltipStyle();
+        UpdateTooltipSize();
+        tooltipRoot.gameObject.SetActive(true);
+        tooltipRoot.SetAsLastSibling();
+        UpdateTooltipPosition();
+    }
+
+    private void HideLinkTooltip()
+    {
+        if (activeTooltipOwner != null && activeTooltipOwner != this)
+            return;
+
+        HideTooltipImmediate();
+    }
+
+    private void HideTooltipImmediate()
+    {
+        if (tooltipRoot != null)
+            tooltipRoot.gameObject.SetActive(false);
+
+        activeTooltipOwner = null;
+    }
+
+    private void EnsureTooltipExists()
+    {
+        Canvas canvas = GetTooltipCanvas();
+        if (canvas == null)
+            return;
+
+        if (tooltipRoot != null && tooltipCanvas == canvas)
+            return;
+
+        if (tooltipRoot != null)
+        {
+            Destroy(tooltipRoot.gameObject);
+        }
+
+        tooltipCanvas = canvas;
+
+        GameObject tooltipObject = new GameObject("LinkHoverTooltip", typeof(RectTransform), typeof(Image));
+        tooltipObject.transform.SetParent(canvas.transform, false);
+
+        tooltipRoot = tooltipObject.GetComponent<RectTransform>();
+        tooltipRoot.anchorMin = new Vector2(0.5f, 0.5f);
+        tooltipRoot.anchorMax = new Vector2(0.5f, 0.5f);
+        tooltipRoot.pivot = new Vector2(0f, 1f);
+
+        tooltipBackground = tooltipObject.GetComponent<Image>();
+        tooltipBackground.sprite = Resources.GetBuiltinResource<Sprite>("UI/Skin/Background.psd");
+        tooltipBackground.type = Image.Type.Sliced;
+        tooltipBackground.color = new Color(0.09f, 0.11f, 0.15f, 0.94f);
+        tooltipBackground.raycastTarget = false;
+
+        GameObject labelObject = new GameObject("Label", typeof(RectTransform), typeof(TextMeshProUGUI));
+        labelObject.transform.SetParent(tooltipObject.transform, false);
+
+        RectTransform labelRect = labelObject.GetComponent<RectTransform>();
+        labelRect.anchorMin = Vector2.zero;
+        labelRect.anchorMax = Vector2.one;
+        labelRect.offsetMin = new Vector2(TooltipPadding.x * 0.5f, TooltipPadding.y * 0.5f);
+        labelRect.offsetMax = new Vector2(-TooltipPadding.x * 0.5f, -TooltipPadding.y * 0.5f);
+
+        tooltipLabel = labelObject.GetComponent<TextMeshProUGUI>();
+        tooltipLabel.enableWordWrapping = true;
+        tooltipLabel.overflowMode = TextOverflowModes.Overflow;
+        tooltipLabel.alignment = TextAlignmentOptions.Left;
+        tooltipLabel.raycastTarget = false;
+        tooltipLabel.text = string.Empty;
+
+        UpdateTooltipStyle();
+        tooltipRoot.gameObject.SetActive(false);
+    }
+
+    private Canvas GetTooltipCanvas()
+    {
+        if (parentCanvas == null)
+        {
+            parentCanvas = GetComponentInParent<Canvas>();
+            if (parentCanvas == null)
+                parentCanvas = FindAnyObjectByType<Canvas>();
+        }
+
+        return parentCanvas;
+    }
+
+    private void UpdateTooltipStyle()
+    {
+        if (tooltipLabel == null || pageText == null)
+            return;
+
+        if (pageText.font != null)
+            tooltipLabel.font = pageText.font;
+
+        tooltipLabel.fontSize = Mathf.Max(20f, pageText.fontSize * 0.55f);
+        tooltipLabel.color = Color.white;
+    }
+
+    private void UpdateTooltipSize()
+    {
+        if (tooltipRoot == null || tooltipLabel == null)
+            return;
+
+        Vector2 preferredSize = tooltipLabel.GetPreferredValues(tooltipLabel.text, TooltipMaxWidth, 0f);
+        float width = Mathf.Min(TooltipMaxWidth, preferredSize.x) + TooltipPadding.x;
+        float height = preferredSize.y + TooltipPadding.y;
+
+        tooltipRoot.sizeDelta = new Vector2(width, height);
+    }
+
+    private void UpdateTooltipPosition()
+    {
+        if (tooltipRoot == null || tooltipCanvas == null || !tooltipRoot.gameObject.activeSelf)
+            return;
+
+        RectTransform canvasRect = tooltipCanvas.transform as RectTransform;
+        if (canvasRect == null)
+            return;
+
+        Camera eventCamera = tooltipCanvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : tooltipCanvas.worldCamera;
+        if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, Input.mousePosition, eventCamera, out Vector2 localPoint))
+            return;
+
+        Vector2 targetPosition = localPoint + TooltipOffset;
+        Vector2 tooltipSize = tooltipRoot.rect.size;
+        Rect canvasBounds = canvasRect.rect;
+
+        targetPosition.x = Mathf.Clamp(targetPosition.x, canvasBounds.xMin, canvasBounds.xMax - tooltipSize.x);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, canvasBounds.yMin + tooltipSize.y, canvasBounds.yMax);
+
+        tooltipRoot.anchoredPosition = targetPosition;
+    }
+
+    private string ExtractArticleTitle(string link)
+    {
+        if (string.IsNullOrWhiteSpace(link))
+            return string.Empty;
+
+        string normalizedLink = CloseParentheses(link);
+        Match match = Regex.Match(normalizedLink, WikipediaArticlePattern);
+        if (!match.Success)
+            return normalizedLink;
+
+        string rawTitle = match.Groups[1].Value.Replace('_', ' ');
+        return Uri.UnescapeDataString(rawTitle);
     }
 
     private void PlayClickSoundAtUser()
